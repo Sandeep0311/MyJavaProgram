afeb7a9b136f345558eb04d5fec596742fb231a7
Sandeep0311 <sandeep.raj@tothenew.com>
2018-02-13 15:02:47 +0530

    constructor added

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,5 +1,18 @@
 public class Demo2 {
+    int i;
+    char c;
+    String s;
+    boolean b;
+
+    Demo2(){
+        System.out.println(i);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(b);
+    }
     public static void main(String[] args) {
         System.out.println("Hello this is demo2");
+        Demo2 d=new Demo2();
+
     }
 }
